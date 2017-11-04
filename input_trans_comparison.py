79ea2de03e0c98dde10c30fe7d54ff5f195a4ca4
Nils Wisiol <info@nils-wisiol.de>
2017-11-04 09:21:28 +0100

    Removed unneccessary experiment from list to speed things up

--- input_trans_comparison.py
+++ input_trans_comparison.py
@@ -9,7 +9,6 @@ n = 64
 k = 4
 
 experiment_matrix = {
-    LTFArray.transform_atf:                                                [2, 5, 10, 20, 50, 100, 150,                          1000],
     LTFArray.transform_lightweight_secure_original:                        [2,    10,     50, 100,      200, 300, 400,           1000],
     LTFArray.generate_random_permutation_transform(0xbad, n, k, atf=True): [2,            50, 100,      200,      400, 600,      1000, 2000],
     LTFArray.generate_random_permutation_transform(0xa55, n, k, atf=True): [2,            50, 100,      200,      400, 600,      1000, 2000],
