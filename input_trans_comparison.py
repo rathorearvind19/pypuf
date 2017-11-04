7149ce06631ab9e4e7f94e470a10e914354e6a09
Nils Wisiol <info@nils-wisiol.de>
2017-11-04 08:57:47 +0100

    Experiment configuration for comparison of random, random permutation

--- input_trans_comparison.py
+++ input_trans_comparison.py
@@ -5,11 +5,14 @@ from pypuf.experiments.experimenter import Experimenter
 from pypuf.simulation.arbiter_based.ltfarray import LTFArray
 
 
+n = 64
+k = 4
+
 experiment_matrix = {
-    0: [2, 5, 10, 20, 50, 100, 150,                          1000],
-    1: [2,    10,     50, 100,      200, 300, 400,           1000],
-    2: [2,            50, 100,      200,      400, 600,      1000],
-    3: [2,            50,           200,      400, 600, 800, 1000],
+    LTFArray.transform_atf:                                                [2, 5, 10, 20, 50, 100, 150,                          1000],
+    LTFArray.transform_lightweight_secure_original:                        [2,    10,     50, 100,      200, 300, 400,           1000],
+    LTFArray.generate_random_permutation_transform(0xbad, n, k, atf=True): [2,            50, 100,      200,      400, 600,      1000, 2000],
+    LTFArray.generate_random_permutation_transform(0xa55, n, k, atf=True): [2,            50, 100,      200,      400, 600,      1000, 2000],
 }
 
 sample_size = 1000
@@ -20,20 +23,15 @@ seed_instance = 0xc0ffee
 seed_model = seed_instance + 1
 
 for _ in range(sample_size):
-    for row_index in experiment_matrix:
-        transformation = LTFArray.generate_stacked_transform(
-            LTFArray.transform_random,
-            row_index,
-            LTFArray.transform_atf,
-        )
-        for N in experiment_matrix[row_index]:
+    for transformation in experiment_matrix:
+        for N in experiment_matrix[transformation]:
             seed_instance += 2
             seed_model += 2
             experiments.append(
                 ExperimentLogisticRegression(
                     log_name=log_name,
-                    n=64,
-                    k=4,
+                    n=n,
+                    k=k,
                     N=N*1000,
                     seed_instance=seed_instance,
                     seed_model=seed_model,
