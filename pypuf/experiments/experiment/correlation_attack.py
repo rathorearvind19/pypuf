4ccdfe3dfcd08aa1d0e8b49c92479e005d28fb19
Nils Wisiol <info@nils-wisiol.de>
2017-11-09 09:20:34 +0100

    Ports correlation attack into pypuf experimenter framework

---
+++ pypuf/experiments/experiment/correlation_attack.py
@@ -0,0 +1,91 @@
+from numpy.random import RandomState
+from numpy.linalg import norm
+from pypuf.experiments.experiment.base import Experiment
+from pypuf.learner.regression.correlation_attack import CorrelationAttack
+from pypuf.simulation.arbiter_based.ltfarray import LTFArray
+from pypuf.tools import TrainingSet, approx_dist
+
+
+class ExperimentCorrelationAttack(Experiment):
+
+    def __init__(self, n, k,
+                 log_name,
+                 seed_model,
+                 seed_instance,
+                 seed_challenge,
+                 seed_challenge_distance,
+                 N,
+                 ):
+        super().__init__(
+            log_name='%s.0x%x_0x%x_0_%i_%i_%i_%s_%s' % (
+                log_name,
+                seed_model,
+                seed_instance,
+                n,
+                k,
+                N,
+                LTFArray.transform_lightweight_secure_original.__name__,
+                LTFArray.combiner_xor.__name__,
+            ),
+        )
+        self.n = n
+        self.k = k
+        self.N = N
+        self.seed_instance = seed_instance
+        self.instance_prng = RandomState(seed=self.seed_instance)
+        self.seed_model = seed_model
+        self.model_prng = RandomState(seed=self.seed_model)
+        self.combiner = LTFArray.combiner_xor
+        self.transformation = LTFArray.transform_lightweight_secure_original
+        self.seed_challenge = seed_challenge
+        self.challenge_prng = RandomState(self.seed_challenge)
+        self.seed_chl_distance = seed_challenge_distance
+        self.distance_prng = RandomState(self.seed_chl_distance)
+        self.instance = None
+        self.learner = None
+        self.model = None
+
+    def run(self):
+        # TODO input transformation is computed twice. Add a shortcut to recycle results from the first computation
+        self.instance = LTFArray(
+            weight_array=LTFArray.normal_weights(self.n, self.k, random_instance=self.instance_prng),
+            transform=self.transformation,
+            combiner=self.combiner,
+        )
+        self.learner = CorrelationAttack(
+            n=self.n,
+            k=self.k,
+            training_set=TrainingSet(instance=self.instance, N=self.N, random_instance=self.challenge_prng),
+            validation_set=TrainingSet(instance=self.instance, N=10000, random_instance=self.distance_prng),
+            weights_prng=self.model_prng,
+            logger=self.progress_logger,
+        )
+        self.model = self.learner.learn()
+
+    def analyze(self):
+        """
+        Analyzes the learned result.
+        """
+        assert self.model is not None
+
+        self.result_logger.info(
+            # seed_instance  seed_model n      k      N      time   initial_iterations initial_accuracy best_accuracy accuracy model values
+            '0x%x\t'        '0x%x\t'   '%i\t' '%i\t' '%i\t' '%f\t' '%i\t'             '%f\t'           '%f\t'        '%f\t'  '%s',
+            self.seed_instance,
+            self.seed_model,
+            self.n,
+            self.k,
+            self.N,
+            self.measured_time,
+            self.learner.initial_iterations,
+            self.learner.initial_accuracy,
+            self.learner.best_accuracy,
+            1.0 - approx_dist(
+                self.instance,
+                self.model,
+                min(10000, 2 ** self.n),
+                random_instance=self.distance_prng,
+            ),
+            ','.join(map(str, self.model.weight_array.flatten() / norm(self.model.weight_array.flatten())))
+
+        )
---
+++ pypuf/learner/regression/correlation_attack.py
@@ -0,0 +1,137 @@
+"""
+This module provides an attack on XOR Arbiter PUFs that is based off known correlation in sub-challenge generation of
+the input transformation.
+"""
+from itertools import permutations
+from scipy.io import loadmat
+from numpy.random import RandomState
+from numpy import empty, roll
+from pypuf.learner.base import Learner
+from pypuf.learner.regression.logistic_regression import LogisticRegression
+from pypuf.simulation.arbiter_based.ltfarray import LTFArray
+from pypuf.tools import set_dist
+
+
+class CorrelationAttack(Learner):
+
+    OPTIMIZATION_ACCURACY_LOWER_BOUND = .65
+    OPTIMIZATION_ACCURACY_UPPER_BOUND = .95
+    OPTIMIZATION_ACCURACY_GOAL = .99
+
+    def __init__(self, n, k,
+                 training_set, validation_set,
+                 weights_mu=0, weights_sigma=1, weights_prng=RandomState(),
+                 initial_model_iteration_limit=100,
+                 lr_iteration_limit=1000,
+                 logger=None, bias=False):
+        self.n = n
+        self.k = k
+
+        self.training_set = training_set
+        self.validation_set = validation_set
+
+        self.weights_mu = weights_mu
+        self.weights_sigma = weights_sigma
+        self.weights_prng = weights_prng
+
+        self.logger = logger
+        self.initial_model_iteration_limit = initial_model_iteration_limit
+        self.bias = bias
+
+        self.lr_learner = LogisticRegression(
+            t_set=training_set,
+            n=n,
+            k=k,
+            transformation=LTFArray.transform_lightweight_secure_original,
+            combiner=LTFArray.combiner_xor,
+            weights_mu=weights_mu,
+            weights_sigma=weights_sigma,
+            weights_prng=weights_prng,
+            logger=logger,
+            iteration_limit=lr_iteration_limit,
+            bias=False
+        )
+        
+        self.initial_accuracy = .5
+        self.initial_iterations = 0
+
+        assert n in (64, 128), 'Correlation attack for %i bit is currently not supported.' % n
+        assert validation_set.N >= 2000, 'Validation set should contain at least 2000 challenges'
+
+        self.correlation_permutations = loadmat(
+            'correlation_permutations_lightweight_secure_original_%i_10.mat' % n
+        )['shiftOverviewData'][:, :, 0].astype('int64')
+
+    def learn(self):
+        # Try to find an initial model with accuracy better than the lower bound.
+        self.initial_accuracy = .5
+        self.initial_iterations = 0
+        while self.initial_accuracy < self.OPTIMIZATION_ACCURACY_LOWER_BOUND and \
+                        self.initial_iterations < self.initial_model_iteration_limit:
+            initial_model = self.lr_learner.learn()
+            self.initial_accuracy = 1 - set_dist(initial_model, self.validation_set)
+
+        self.best_model = initial_model
+        self.best_accuracy = self.initial_accuracy
+
+        if self.initial_accuracy < self.OPTIMIZATION_ACCURACY_LOWER_BOUND:
+            self.logger.debug('Model accuracy on the validation %.2f set after %i learning attempts still below %.2f, '
+                              'giving up' % (self.initial_accuracy, self.initial_model_iteration_limit,
+                                             self.OPTIMIZATION_ACCURACY_LOWER_BOUND))
+            return initial_model
+
+        if self.initial_accuracy > self.OPTIMIZATION_ACCURACY_UPPER_BOUND:
+            self.logger.debug('We don\'t need to start the optimization as we already have a model with validation set '
+                              'accuracy of %.2f' % self.initial_accuracy)
+            return initial_model
+
+        # Try all permutations with high initial accuracy and see if any of them lead to a good finial result
+        adopted_weights = self.find_high_accuracy_weight_permutations(initial_model.weight_array, self.initial_accuracy)
+        self.logger.debug('Trying %i permuted weights.' % len(adopted_weights))
+        for weights in adopted_weights:
+            model = self.lr_learner.learn(init_weight_array=weights)
+            accuracy = 1 - set_dist(model, self.validation_set)
+            self.logger.debug('With a permutation, after restarting the learning we achieved accuracy %.2f!' % accuracy)
+            if accuracy > self.best_accuracy:
+                self.best_model = model
+                self.best_accuracy = accuracy
+            else:
+                self.logger.debug('Learning after permuting lead to accuracy %.2f, no improvement :-(' % accuracy)
+            if accuracy > self.OPTIMIZATION_ACCURACY_GOAL:
+                self.logger.debug('Found a model with accuracy better than %.2f, aborting.' %
+                                  self.OPTIMIZATION_ACCURACY_GOAL)
+                return model
+
+        self.logger.debug('After trying all permutations, we found a model with accuracy %.2f.' % self.best_accuracy)
+        return self.best_model
+
+    def find_high_accuracy_weight_permutations(self, weights, threshold):
+        high_accuracy_permutations = []
+        for permutation in list(permutations(range(self.k)))[1:]:
+            adopted_weights = self.adopt_weights(weights, permutation)
+            adopted_instance = LTFArray(
+                weight_array=adopted_weights[:, :-1],
+                transform=LTFArray.transform_lightweight_secure_original,
+                combiner=LTFArray.combiner_xor,
+                bias=adopted_weights[:,-1:]
+            )
+            accuracy = 1 - set_dist(adopted_instance, self.validation_set)
+            self.logger.debug('For permutation %s, we have accuracy %.4f' % (permutation, accuracy))
+            if accuracy >= threshold:
+                high_accuracy_permutations.append(
+                    {
+                        'accuracy': accuracy,
+                        'permutation': permutation,
+                        'weights': adopted_instance.weight_array,
+                    }
+                )
+
+        high_accuracy_permutations.sort(key=lambda x: x['accuracy'])
+        return [ item['weights'] for item in high_accuracy_permutations ]
+
+    def adopt_weights(self, weights, permutation):
+        adopted_weights = empty(weights.shape)
+        for l in range(self.k):
+            adopted_weights[permutation[l], :] = \
+                roll(weights[l, :], self.correlation_permutations[l, permutation[l]])
+        return adopted_weights
