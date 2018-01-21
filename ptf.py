90c8261b8a83e65bde6809bdc10382057a881e2c
Nils Wisiol <info@nils-wisiol.de>
2018-01-21 12:26:21 +0100

    rework ptf length into a pypuf experiment

--- ptf.py
+++ ptf.py
@@ -1,11 +1,15 @@
 import sys
-from numpy import array, ndarray
-from numpy.random import RandomState, normal
+from random import sample
+from string import ascii_uppercase
+from numpy import array
+from numpy.random import RandomState, normal, shuffle
 from itertools import product
 from datetime import datetime, timedelta
+from pypuf.experiments.experiment.base import Experiment
+from pypuf.experiments.experimenter import Experimenter
 
 
-class PTFMonomials():
+class PTFMonomialsExperiment(Experiment):
 
     @staticmethod
     def monomials_random(n, k, random_instance):
@@ -21,19 +25,27 @@ class PTFMonomials():
             for _ in range(k)
         ])
 
-    def __init__(self, n, k, monomials, random_instance=None):
-
+    def __init__(self, log_name, n, k, monomials, seed):
+        super().__init__(
+            log_name='%s.0x%x_%i_%i_%s' % (
+                log_name,
+                seed,
+                n,
+                k,
+                monomials.__name__,
+            ),
+        )
         self.n = n
         self.k = k
-        self.random_instance = random_instance
+        self.seed = seed
+        self.random_instance = RandomState(seed)
+        self.monomials = monomials
+        self.S_coll = monomials(n, k, self.random_instance)
 
-        # use dead beef by default
-        if random_instance is None:
-            random_instance = RandomState(0xdeadbeef)
-
-        # random sets S for each LTF input bit (k*n many)
-        #S_coll = random_instance.choice((0, 1), (k,n,n))
-        S_coll = monomials(n, k, random_instance)
+    def run(self):
+        S_coll = self.S_coll
+        n = self.n
+        k = self.k
 
         # random weights (k*n many)
         w_coll = normal(size=(k,n))
@@ -41,15 +53,15 @@ class PTFMonomials():
         # compute PTF self.monomials by expanding the product
         # prod_(l=1)^k sum_(i=1)^n w_(l,i) chi_(S_(l,i))
         # where chi_(S_(l,i)) is the product of the input coordiantes with indices that occur in S_(l,i)
-        self.monomials = {}
+        self.ptf_monomials = {}
         c = 0
         start_time = datetime.now()
         for indices in product(range(n), repeat=k):
-            if (c + 1) % 50000 == 0:
-                progress = c / n**k
-                elapsed_time = datetime.now() - start_time
-                remaining_time = timedelta(seconds=(elapsed_time / progress).total_seconds() // 15 * 15) if progress > 0 else '???'
-                sys.stdout.write("\rprogress: %.4f (%s/%s) remaining time: %s                 " % (c/(n**k), c, n**k, remaining_time))
+            #if (c + 1) % 50000 == 0:
+            #    progress = c / n**k
+            #    elapsed_time = datetime.now() - start_time
+            #    remaining_time = timedelta(seconds=(elapsed_time / progress).total_seconds() // 15 * 15) if progress > 0 else '???'
+            #    sys.stdout.write("\rprogress: %.4f (%s/%s) remaining time: %s                 " % (c/(n**k), c, n**k, remaining_time))
             c += 1
             #print("Combination %s:" % (indices,))
             running_s = [0] * n  #zeros(n, dtype='int8')
@@ -63,21 +75,35 @@ class PTFMonomials():
             #print()
 
             running_s.flags.writeable = False
-            hash = running_s.data.tobytes()
-            if hash in self.monomials:
-                self.monomials[hash] += running_w
+            monomial_key = running_s.data.tobytes()
+            if monomial_key in self.ptf_monomials:
+                self.ptf_monomials[monomial_key] += running_w
             else:
-                self.monomials[hash] = running_w
+                self.ptf_monomials[monomial_key] = running_w
 
         #for hash in sorted(self.monomials):
         #    print('%s: %.3f' % (hash, self.monomials[hash]))
 
     def ptf_length(self):
-        return len(self.monomials)
+        return len(self.ptf_monomials)
 
     def ptf_length_relative(self):
         return self.ptf_length() / (2**self.n)
 
+    def analyze(self):
+        assert self.ptf_monomials is not None
+
+        self.result_logger.info(
+            # seed   n      k      monomials abslen rellen    time
+            '0x%x\t' '%i\t' '%i\t' '%s\t'    '%i\t' '%.12f\t' '%.12f\t',
+            self.seed,
+            self.n,
+            self.k,
+            self.monomials.__name__,
+            self.ptf_length(),
+            self.ptf_length_relative(),
+            self.measured_time,
+        )
 
 def main(args):
 
@@ -85,30 +111,32 @@ def main(args):
                     6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 8, 9, 9, 9, 9, \
                     9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 11, 11, 11, 11, 11, 11 ]
 
-    relative_length_rand = []
-    absolute_length_rand = []
-    relative_length_classic = []
-    absolute_length_classic = []
+    master_seed = 0xbadeaffe
+    sample_size = 100
+    n_max = 16
+    experiments = []
+    log_name = ''.join(sample(list(ascii_uppercase), 5))
 
-    for n in range(2, 15):
-        k = sufficient_k[n]
-        rand_puf = PTFMonomials(n, k, monomials=PTFMonomials.monomials_random)
-        classic_puf = PTFMonomials(n, k, monomials=PTFMonomials.monomials_atf)
-        relative_length_rand += [rand_puf.ptf_length_relative()]
-        absolute_length_rand += [rand_puf.ptf_length()]
-        relative_length_classic += [classic_puf.ptf_length_relative()]
-        absolute_length_classic += [classic_puf.ptf_length()]
-        sys.stdout.flush()
-        print("\r\rn: %i, k: %i, relative length: %.4f (rand) vs %.4f (classic)" % (n, k, rand_puf.ptf_length_relative(), classic_puf.ptf_length_relative()))
+    seed = master_seed
+    for n in range(2, n_max):
+        for _ in range(sample_size):
+            for monomials in [
+                PTFMonomialsExperiment.monomials_atf,
+                PTFMonomialsExperiment.monomials_random,
+            ]:
+                experiments.append(
+                    PTFMonomialsExperiment(
+                        log_name=log_name,
+                        n=n,
+                        k=sufficient_k[n],
+                        monomials=monomials,
+                        seed=seed,
+                    )
+                )
+                seed += 1
 
-    print(relative_length_rand)
-    print(absolute_length_rand)
-    print(relative_length_classic)
-    print(absolute_length_classic)
-
-
-
-
+    shuffle(experiments)
+    Experimenter(log_name, experiments).run()
 
 
 if __name__ == '__main__':
