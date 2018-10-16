9cb7fb496185baa4d55ac278eef0fcbe3720062d
Chrism7 <chrism@mi.fu-berlin.de>
2018-10-16 16:29:00 +0200

    implement search for good permutation like Mispan et.al.

---
+++ random_permutation.py
@@ -0,0 +1,68 @@
+import numpy as np
+
+
+RESULT_TYPE = np.int8
+
+
+def swap_every_bit(challenge):
+    # Calculate challenges by separately swapping every bit of given challenge
+    n = len(challenge)
+    c_swapped = np.tile(challenge, (n, 1))
+    np.fill_diagonal(c_swapped, c_swapped.diagonal() * -1)
+    return c_swapped
+
+
+def create_swapped_challenges(challenges):
+    # Calculate all challenges with hamming distance to given challenges equals 1
+    num, n = np.shape(challenges)
+    challenges_swapped = np.empty([num, n, n], dtype=RESULT_TYPE)
+    for i in range(num):
+        challenges_swapped[i] = swap_every_bit(challenges[i])
+    return challenges_swapped
+
+
+def calc_delta(probs):
+    # Calculate delta := mean + sd of differences of consecutive output transition probabilities
+    n = len(probs)
+    diffs = np.empty(n - 1)
+    for i in range(n - 1):
+        diffs[i] = probs[i+1] - probs[i]
+    return np.mean(diffs) + np.std(diffs)
+
+
+def calc_probs(instance, challenges, permutation):
+    """ Calculate the output transition probability (actually frequency) for each index
+    over all given challenges on given instance using given permutation """
+    probs = np.zeros(instance.n)
+    permuted_cs = challenges[:, permutation]
+    permuted_cs_swapped = create_swapped_challenges(challenges)[:, :, permutation]
+    responses = instance.eval(permuted_cs)
+    for i, r in enumerate(responses):
+        responses_swapped = instance.eval(permuted_cs_swapped[i])
+        for j, r_swapped in enumerate(responses_swapped):
+            probs[j] += abs(r - r_swapped) / 2
+    return probs / len(challenges)
+
+
+def permutation_is_good(probs, delta):
+    # Check if the permutation is accurate by applying delta as condition
+    n = len(probs)
+    counter = 0
+    for i in range(n - 1):
+        if probs[i + 1] - probs[i] <= delta:
+            counter += 1
+            if counter > 2:
+                return False
+    return True
+
+
+def find_good_permutation(instance, challenges, prng):
+    # Determine a good permutation in the sense of Mispan et. al. (target := 0)
+    tries = 1000
+    for _ in range(tries):
+        permutation = prng.permutation(instance.n)
+        probs = calc_probs(instance, challenges, permutation)
+        delta = calc_delta(probs)
+        if permutation_is_good(probs, delta):
+            return permutation
+    return None
---
+++ test_random_permutation.py
@@ -0,0 +1,90 @@
+from pypuf.simulation.arbiter_based.ltfarray import LTFArray
+from pypuf.tools import sample_inputs
+from random_permutation import *
+import datetime
+
+
+# Randomly choose seed
+seed = np.random.randint(0xffff)
+print('seed = ', seed)
+
+# Create PUF instance
+n = 16
+k = 1
+prng_i = np.random.RandomState(seed)
+transformation = LTFArray.transform_id
+combiner = LTFArray.combiner_xor
+
+instance = LTFArray(
+    weight_array=LTFArray.normal_weights(n, k, random_instance=prng_i),
+    transform=transformation,
+    combiner=combiner
+)
+
+time_start = datetime.datetime.now()
+
+
+# Sample challenges
+num = 32000
+prng_c = np.random.RandomState(seed+1)
+challenges = sample_inputs(n, num, prng_c)
+time_sampling = datetime.datetime.now()
+
+
+# Create swapped challenges
+cs_swapped = create_swapped_challenges(challenges)
+time_swapping = datetime.datetime.now()
+
+
+# Permute challenges
+prng_p = np.random.RandomState(seed+2)
+permutation = prng_p.permutation(n)
+permuted_cs = challenges[:, permutation]
+permuted_cs_swapped = cs_swapped[:, :, permutation]
+time_permuting = datetime.datetime.now()
+
+
+# Calculate output transition probabilities
+responses = instance.eval(permuted_cs)
+probs = np.zeros(instance.n)
+for i, r in enumerate(responses):
+    responses_swapped = instance.eval(permuted_cs_swapped[i])
+    for j, r_swapped in enumerate(responses_swapped):
+        probs[j] += abs(r - r_swapped) / 2
+time_evaluating = datetime.datetime.now()
+
+
+# Calculate condition delta
+delta = calc_delta(probs)
+time_delta = datetime.datetime.now()
+
+
+# Check if permutation is good
+check = permutation_is_good(probs, delta)
+time_checking = datetime.datetime.now()
+
+
+times = list()
+times.append(time_sampling - time_start)
+times.append(time_swapping - time_sampling)
+times.append(time_permuting - time_swapping)
+times.append(time_evaluating - time_permuting)
+times.append(time_delta - time_evaluating)
+times.append(time_checking - time_delta)
+times.append(time_checking - time_start)
+
+print('\nsampling\t',   times[0], '\t', 100*round(times[0]/times[6], 3), '%',
+      '\nswapping\t',   times[1], '\t', 100*round(times[1]/times[6], 3), '%',
+      '\npermuting\t',  times[2], '\t', 100*round(times[2]/times[6], 3), '%',
+      '\nevaluating\t', times[3], '\t', 100*round(times[3]/times[6], 3), '%',
+      '\ndelta\t\t',    times[4], '\t', 100*round(times[4]/times[6], 3), '%',
+      '\nchecking\t',   times[5], '\t', 100*round(times[5]/times[6], 3), '%',
+      '\n\ntotal\t\t',  times[6], '\t', 100*round(times[6]/times[6], 3), '%')
+
+
+"""
+prng_p = np.random.RandomState(seed+2)
+permutation = find_good_permutation(instance, challenges, prng_p)
+
+print(permutation)
+"""
